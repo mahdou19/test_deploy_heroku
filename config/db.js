83ce01fee638b9a06a628a515accbe5ed7040e72
Mamadou <mamadou.sall@42c.fr>
2022-10-06 16:13:15 +0200

    config database

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    "mongodb+srv://mahdou:Mahdou123@deploydatabase.veip9ur.mongodb.net/BACK_PROJECT"
+    "mongodb+srv://mahdou:Mahdou123@deploydatabase.veip9ur.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Failed to connect to MongoDB', err));
