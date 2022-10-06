b9140c8b4a097106a4d2f657b2ebaf2ce056a3a3
Mamadou <mamadou.sall@42c.fr>
2022-10-06 15:42:12 +0200

    database config

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb://' +
-      process.env.DB_USER_PASS +
-      '/BACK_PROJECT_1'
+    "mongodb+srv://mahdou:Mahdou123@deploydatabase.veip9ur.mongodb.net/BACK_PROJECT"
   )
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Failed to connect to MongoDB', err));
